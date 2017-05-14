205e76438e4ef1a74196d3b6d6adde08040c8770
huang12396 <911382182@qq.com>
2017-05-14 20:37:40 +0800

    更改链接

---
+++ 工程文件夹/WebApplication1/src/WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            ViewBag.Title = "登录";
+            return View();
+        }
+    }
+}
--- 工程文件夹/WebApplication1/src/WebApplication1/Controllers/HomeController.cs
+++ 工程文件夹/WebApplication1/src/WebApplication1/Controllers/HomeController.cs
@@ -19,6 +19,7 @@ namespace WebApplication1.Controllers
 
             return View();
         }
+      
 
         public IActionResult Contact()
         {
