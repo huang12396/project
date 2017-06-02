bcd60a989cc6d65acf6a7afdcdda9eb1fe673ee3
Gorker <gorkerfish@gmail.com>
2017-06-02 17:41:14 +0800

    首页读取数据库商品

--- 工程文件夹/WebApplication2/src/WebApplication2/Controllers/HomeController.cs
+++ 工程文件夹/WebApplication2/src/WebApplication2/Controllers/HomeController.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DBAlcoholContext db;
+
+        public HomeController(DBAlcoholContext alcoholdb)
+        {
+            db = alcoholdb;
+        }
         public IActionResult Index()
         {
-            return View();
+            ViewBag.Title = "主页";
+
+            HomeIndexViewModel ivm = new HomeIndexViewModel();
+            ivm.recProducts = new List<ProductList>();
+            ivm.disProducts = new List<ProductList>();
+            ivm.productCats = new List<ProductCat>();
+
+            //获取推荐商品(4种)
+            var recProducts = db.Product.Where<Product>(m => m.ProductId > 001).OrderBy<Product, float>(m => (float)m.Price).Take<Product>(4);
+            foreach(var p in recProducts)
+            {
+                ProductList pl = new ProductList();
+                pl.p = new Product { ProductId = p.ProductId, ProductName = p.ProductName, Price = p.Price };
+                ivm.recProducts.Add(pl);
+            }
+            return View(ivm);
         }
 
         public IActionResult About()
---
+++ 工程文件夹/WebApplication2/src/WebApplication2/Models/ViewModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using WebApplication2.Models.AccountViewModels;
+
+namespace WebApplication2.Models
+{
+    public class HomeIndexViewModel
+    {
+        public List<ProductList> recProducts { get; set; }
+        public List<ProductList> disProducts { get; set; }
+        public List<ProductCat> productCats { get; set; }
+    }
+
+    public class ProductList
+    {
+        public Product p { get; set; }
+    }
+    public class ProductCat
+    {
+        public string typeName { get; set; }
+        public List<ProductClassification> types { get; set; }
+    }
+}
