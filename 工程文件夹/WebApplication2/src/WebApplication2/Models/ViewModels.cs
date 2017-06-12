592c07eb101589a2294ec8e40f60378a56fb8b3e
Gorker <gorkerfish@gmail.com>
2017-06-12 10:16:00 +0800

    改了之后能运行了

---
+++ 工程文件夹/WebApplication2/src/WebApplication2/Models/ViewModels.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using WebApplication2.Models.AccountViewModels;
+
+namespace WebApplication2.Models
+{
+    public class HomeIndexViewModel
+    {
+        public List<ProductList> recProducts { get; set; }
+        public List<ProductList> disProducts { get; set; }
+        public List<ProductCat> productCats { get; set; }
+    }
+
+    public class ProductList
+    {
+        public Product p { get; set; }
+        public Ppics pn { get; set; }
+    }
+   
+    public class ProductCat
+    {
+        public string typeName { get; set; }
+        public List<ProductClassification> types { get; set; }
+    }
+
+    public class MemberHomeModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "当前密码")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "{0} 必须至少包含 {2} 个字符。", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "新密码")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "确认新密码")]
+        [Compare("NewPassword", ErrorMessage = "新密码和确认密码不匹配。")]
+        public string ConfirmPassword { get; set; }
+        //public LocalPasswordModel PassWordModel { get; set; }
+        public RegisterViewModel CustomerInfo { get; set; }
+    }
+    public class ProductDtl
+    {
+        public Product p { get; set; }
+        public IEnumerable<Ppics> productpics { get; set; }
+        public Ppics pnd { get; set; }
+        public Pdetail pdtl { get; set; }
+    }
+    public class CartItem
+    {
+        public string ProductName { get; set; }
+        public string feature { get; set; }
+        public decimal Price { get; set; }
+        public int qty { get; set; }
+        public string PicName { get; set; }
+    }
+}
